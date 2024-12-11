0b95a39d3956d561f2f718b9ab4be0fda81faf30
prathmesh <prathmaxxpadwal2@gmail.com>
2024-12-11 15:06:53 +0530

    added exception class and postDto

---
+++ src/main/java/blogApp/application/springBoot/payload/PostDto.java
@@ -0,0 +1,13 @@
+package blogApp.application.springBoot.payload;
+
+import lombok.Data;
+
+@Data
+public class PostDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+
+}
