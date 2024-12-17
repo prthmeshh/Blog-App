d83dfb6fbd1db0d08ea0fe69ead08b6c3be43b91
prathmesh <prathmaxxpadwal2@gmail.com>
2024-12-17 12:04:26 +0530

    added JWT token Provider Utility Class

--- src/main/java/blogApp/application/springBoot/Security/JwtTokenProvider.java
+++ src/main/java/blogApp/application/springBoot/Security/JwtTokenProvider.java
@@ -1,4 +1,78 @@
 package blogApp.application.springBoot.Security;
 
+import blogApp.application.springBoot.exception.BlogAPIException;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.UnsupportedJwtException;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.security.Key;
+import java.util.Date;
+
+@Component
 public class JwtTokenProvider {
+
+    @Value("${app.jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${app-jwt-expiration-milliseconds}")
+    private long jwtExpirationDate;
+
+
+    //generate Token
+    public String generateToken(Authentication authentication){
+        String username = authentication.getName();
+        Date currentDate = new Date();
+
+        Date expireDate = new Date(currentDate.getTime()+jwtExpirationDate);
+        String token = Jwts.builder()
+                .subject(username)
+                .issuedAt(new Date())
+                .expiration(expireDate)
+                .signWith(key())
+                .compact();
+
+        return token;
+    }
+
+    private Key key(){
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    //get username from jwt token
+    public String getUsername(String token){
+        return Jwts.parser().verifyWith((SecretKey) key()).build()
+                .parseSignedClaims(token)
+                .getPayload()
+                .getSubject();
+    }
+
+    //validate JWT token
+    public boolean validateToken(String token){
+        try{
+            Jwts.parser().verifyWith((SecretKey) key())
+                    .build()
+                    .parse(token);
+
+        }catch(MalformedJwtException malformedJwtException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"Invalid JWT token");
+        }catch(ExpiredJwtException expiredJwtException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"Expired JWT token");
+        }catch(UnsupportedJwtException unsupportedJwtException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"Unsupported JWT token");
+        }catch(IllegalArgumentException illegalArgumentException){
+            throw new BlogAPIException(HttpStatus.BAD_REQUEST,"String is null or empty");
+        }
+
+
+        return true;
+    }
+
 }
