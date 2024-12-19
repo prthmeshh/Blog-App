b1c2828ad634271747c246e581d312f31afc980c
prathmesh <prathmaxxpadwal2@gmail.com>
2024-12-19 10:19:20 +0530

    adding enviornments profile and deploying on aws

--- src/main/java/blogApp/application/springBoot/Application.java
+++ src/main/java/blogApp/application/springBoot/Application.java
@@ -1,8 +1,12 @@
 package blogApp.application.springBoot;
 
+import blogApp.application.springBoot.entity.Role;
+import blogApp.application.springBoot.repository.RoleRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -15,17 +19,30 @@ import org.springframework.context.annotation.Bean;
 				version = "v1.0"
 		)
 )
-public class Application {
+public class Application implements CommandLineRunner {
 
 	@Bean
 	public ModelMapper modelMapper(){
 		return new ModelMapper();
 	}
 
+	@Autowired
+	private RoleRepository roleRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
 
 
+	@Override
+	public void run(String... args) throws Exception {
+		Role adminRole = new Role();
+		adminRole.setName("ROLE_ADMIN");
+		roleRepository.save(adminRole);
 
+		Role userRole = new Role();
+		userRole.setName("ROLE_USER");
+		roleRepository.save(userRole);
+
+	}
 }
